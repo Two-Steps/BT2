11023b854eddd0d1f53d5326535feaced702441b
TungPham <tungphamepu@gmail.com>
2022-06-24 19:33:47 +0700

    second

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,7 +4,9 @@ s = int(se_list[0])
 e = int(se_list[1])
 if s < e:
     for i in range(s,e+1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
             print('Fizz')
         elif i % 5 == 0:
             print('Buzz')
